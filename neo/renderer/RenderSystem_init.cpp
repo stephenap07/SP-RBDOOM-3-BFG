0e494dae9d7e265373b0d4c8ece7c1e9e2117038
Codes4Fun <leyland.needham@gmail.com>
2016-11-21 14:59:43 -0800

    Default Response Curve to Linear

--- neo/renderer/RenderSystem_init.cpp
+++ neo/renderer/RenderSystem_init.cpp
@@ -295,7 +295,7 @@ idCVar vr_strafing( "vr_strafing", "1", CVAR_ARCHIVE | CVAR_BOOL, "enable/disabl
 idCVar vr_forwardOnly( "vr_forwardOnly", "0", CVAR_ARCHIVE | CVAR_BOOL, "left touchpad only moves forward" );
 idCVar vr_maxRadius( "vr_maxRadius", "0.9", CVAR_ARCHIVE | CVAR_FLOAT, "smaller values make it easier to hit max movement speed" );
 idCVar vr_turning( "vr_turning", "0", CVAR_ARCHIVE | CVAR_BOOL, "0 no turning | 1 touch turning" );
-idCVar vr_responseCurve( "vr_responseCurve", "-1", CVAR_ARCHIVE | CVAR_FLOAT, "interpoloate between linear and square curves, -1 for inverse square" );
+idCVar vr_responseCurve( "vr_responseCurve", "0", CVAR_ARCHIVE | CVAR_FLOAT, "interpoloate between linear and square curves, -1 for inverse square" );
 idCVar vr_moveMode( "vr_moveMode", "0", CVAR_ARCHIVE | CVAR_INTEGER, "	0 touch walk | 1 touch walk & hold run | 2 touch walk & click run | 3 click walk | 4 click walk & hold run | 5 click walk & double click run | 6 hold walk" );
 
 const char* fileExten[3] = { "tga", "png", "jpg" };
