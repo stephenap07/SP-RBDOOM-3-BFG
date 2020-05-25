b20c8b843d2218aeae13c124387080a5717f3bdf
Robert Beckebans <robert.beckebans@gmail.com>
2020-05-25 11:17:12 +0200

    Allow 32 bit Windows build to work without FFmpeg

--- neo/cmake-vs2019-32bit-windows10.bat
+++ neo/cmake-vs2019-32bit-windows10.bat
@@ -3,5 +3,5 @@ del /s /q build
 mkdir build
 cd build
 set PATH="C:\Program Files\CMake\bin\";%PATH%
-cmake -G "Visual Studio 16" -A Win32 -DCMAKE_INSTALL_PREFIX=../bin/windows10-32 -DWINDOWS10=ON ../neo
+cmake -G "Visual Studio 16" -A Win32 -DWINDOWS10=ON -DFFMPEG=OFF -DBINKDEC=ON ../neo 
 pause
